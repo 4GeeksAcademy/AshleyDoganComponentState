28d367d5b4330544a7ba23c2872274d6f67dac1f
Ashley Dogan <a.a.rappette@gmail.com>
2025-01-24 23:50:26 +0000

    practice

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,24 @@
 import React from "react";
+import { useState } from "react";
+
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [count,setCount] = useState(0);
 	return (
 		<div className="text-center">
             
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1 className="text-center mt-5">How many puppies do you want? {count}</h1>
+			<button className="more" onClick={() => setCount (count + 1)}>More puppies</button>
+			<button className="less" onClick={() => setCount (count - 1)}>Less puppies</button>
+
+
 		</div>
 	);
 };
